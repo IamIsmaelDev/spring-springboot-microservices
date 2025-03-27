0eb039df9a848da6180a15a1c64b677b69d2249c
CarlosAgustin <carlosagustingarcia@gmail.com>
2025-03-27 10:05:53 +0100

    Fin test controller

--- Retos/Reto 2/src/test/java/com/reto2/controller/AccountServiceControllerTest_MockMvc.java
+++ Retos/Reto 2/src/test/java/com/reto2/controller/AccountServiceControllerTest_MockMvc.java
@@ -26,14 +26,22 @@ class AccountServiceControllerTest_MockMvc {
     @Autowired
     private MockMvc mvc;
 
-/**
     @Test
     public void givenCustomerIdGetAccountsThenStatus200() throws Exception {
-        mvc.perform(get("/account/customer/1").accept(MediaType.APPLICATION_JSON))
+        mvc.perform(get("/account/customer/1")
+                .contentType(MediaType.APPLICATION_JSON) // Incluye el encabezado Content-Type
+                .accept(MediaType.APPLICATION_JSON)) // Indica que esperas JSON en la respuesta
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$[0].balance", is(1000)));
     }
-*/
+
+        @Test
+    public void givenCustomerIdOwnerIdGetAccountsThenStatus400() throws Exception {
+        mvc.perform(get("/account/1/customer/100")
+                .contentType(MediaType.APPLICATION_JSON) // Incluye el encabezado Content-Type
+                .accept(MediaType.APPLICATION_JSON)) // Indica que esperas JSON en la respuesta
+                .andExpect(status().isBadRequest());
+    }
 
 }
