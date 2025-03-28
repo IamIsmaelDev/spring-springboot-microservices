ab8947f8ecbacef39123901e2506d336fd3700d1
Juan Manuel <jmsumilidon@hotmail.com>
2025-03-28 10:35:31 +0100

    testing jwt

--- Retos/Reto 4/src/test/resources/data_testing.sql
+++ Retos/Reto 4/src/test/resources/data_testing.sql
@@ -1,7 +1,11 @@
+DELETE FROM customer;
+
 INSERT INTO customer (id, name, email) VALUES
 (1L, 'Pepe', 'email1@gmail.com'),
 (2L,'Juan', 'email34@gmail.com'),
 (3L, 'Jose', 'email0000@gmail.com');
+
+DELETE FROM accounts;
 
 INSERT INTO accounts (type, opening_date, balance, owner_id) VALUES
 ('Personal', '2024-04-10', 1000, 1L),
